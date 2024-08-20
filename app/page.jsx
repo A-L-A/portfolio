56c87c65559b58cab4703223766173378732a4a0
A-L-A <64477195+A-L-A@users.noreply.github.com>
2024-08-20 20:11:27 +0400

    project setup

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+// import Image from "next/image";
+// import styles from  "styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <Head>
+        <title>Lyse Arlette Aneze Portfolio</title>
+        <meta name="description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <section className="bg-blue-700 h-screen">
+
+        </section>
+      </main>
+    </div>
+  );
+}
