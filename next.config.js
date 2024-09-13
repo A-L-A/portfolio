2d496b61bda9338446df9a59b9051a621f63ff23
A-L-A <64477195+A-L-A@users.noreply.github.com>
2024-09-13 13:25:07 +0200

    added nojekyll

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disables Next.js image optimization for GitHub Pages
   },
-  basePath: "/portfolio", // The GitHub repository name as the base path
-  assetPrefix: "/portfolio/", // Ensures assets are prefixed with the repository name
 };
 
 module.exports = nextConfig;
