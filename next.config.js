38cdd453a1de11c1d06ddb404a80266c1476cd90
A-L-A <64477195+A-L-A@users.noreply.github.com>
2024-09-13 19:39:42 +0200

    Final update for custom domain anezelyse.com

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+  assetPrefix: "",
+  basePath: "",
 };
 
 module.exports = nextConfig;
