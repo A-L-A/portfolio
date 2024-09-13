b3142931851a62604e71b5ff9c8a22c49d7e8f18
A-L-A <64477195+A-L-A@users.noreply.github.com>
2024-09-13 18:31:38 +0200

    Fix React import issues

--- next.config.js
+++ next.config.js
@@ -6,9 +6,19 @@ const nextConfig = {
   },
   assetPrefix: "/portfolio/",
   basePath: "/portfolio",
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
     return config;
+  },
+  compiler: {
+    styledComponents: true,
   },
 };
 
