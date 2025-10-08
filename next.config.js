750fb6e347e1734351bc59678f5ba652cbdd427d
Lyse A. Aneze <64477195+A-L-A@users.noreply.github.com>
2025-10-08 15:44:53 +0000

    Add basePath and trailingSlash for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+   basePath: "/portfolio", 
+   trailingSlash: true,      
   images: {
-    unoptimized: true,
+    unoptimized: true,     
   },
   eslint: {
     ignoreDuringBuilds: true,
